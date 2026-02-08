7e8875e1a6677b3d503f4ce3422bb956f0ef6303
Andrii <m9ta92@gmail.com>
2026-02-08 21:37:52 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,10 @@
 import { defineConfig } from "vite";
-import glob from "glob";
-import injectHTML from "vite-plugin-html-inject";
-import FullReload from "vite-plugin-full-reload";
+import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ command }) => {
-  return {
-    optimizeDeps: {
-      include: ["simplelightbox"],
-    },
-    ssr: {
-      external: ["simplelightbox"],
-    },
-    define: {
-      [command === "serve" ? "global" : "_global"]: {},
-    },
-    root: ".",
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        input: glob.sync("./src/*.html"),
-        output: {
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              return "vendor";
-            }
-          },
-          entryFileNames: "commonHelpers.js",
-        },
-      },
-      outDir: "../../dist",
-    },
-    plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
 });
